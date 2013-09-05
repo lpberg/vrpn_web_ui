bc47a4f21a07ed56e5bce3d42549c8ba9ee73c8a
Leif Berg <lpberg@iastate.edu>
2013-09-05 11:14:43 -0500

    major implementation
    Server sends json to vrpn server over UDP

--- server.py
+++ server.py
@@ -1,22 +1,64 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 7777
+
+
+import json
+
+def encodeButton(num, state):
+	return json.dumps({
+		'type': 2,
+		'button': int(num),
+		'state': state
+	})
+
+def encodeAnalog(num, value):
+	return json.dumps({
+		'type': 3,
+		'num': int(num),
+		'data': value
+	})
+def encodeTracker(num, quat, pos):
+	return json.dumps({
+		'type': 1,
+		'id': int(num),
+		'quat': quat,
+		'pos': pos
+	})
+
+
+def sendToVRPN(message):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.sendto(message, (UDP_IP, UDP_PORT))
+	sock.close()
+
 app = Flask(__name__)
 
-@app.route("/",methods=['POST', 'GET'])
+@app.route("/")
 def hello():
-	if request.method == 'POST':
-		return render_template('index.html', key=request.form['key'])
-	else:
 		return render_template('index.html')
 	
-@app.route("/buttons/<button_name>", methods=['POST', 'GET'])
+@app.route("/button/<button_name>", methods=['POST'])
 def pressButton(button_name):
-	if request.method == 'POST':
-		print "You pressed the %s button" % button_name
-		return render_template('index.html')
+	print("Request data", request.form["state"])
+	if request.form["state"] == "true":
+		data = True
 	else:
-		return render_template('index.html')
+		data = False
+	sendToVRPN(encodeButton(button_name, data))
+	return "OK"
+
+@app.route("/analog/<int:channel>", methods=['POST'])
+def updateAnalog(channel):
+	print("Request data", request.form["state"])
+	data = float(request.form["state"])
+	sendToVRPN(encodeAnalog(channel, data))
+	return "OK"
 
 # @app.route('/text/<string>', methods=['POST', 'GET'])
 # def show_post(string):
