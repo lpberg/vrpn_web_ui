d05b73a5c74e50277b6b29bd25d4b1fc2474c552
Leif Berg <lpberg@iastate.edu>
2013-09-12 08:23:40 -0500

    added slider and flip button objects

--- static/helper.js
+++ static/helper.js
@@ -1,3 +1,51 @@
+function setToggleSwitchBehavior(switchElt, num, buttonsetName, first, second) {
+    switchElt.buttonset();
+	switchElt.change(function () {
+        var buttonstatus = true;
+        var console_string = "";
+        var value = $(this).find(":checked").attr('data-vrpn-button-state');
+        if (value == "true") {
+            buttonstatus = true;
+            console_string = first;
+        } else {
+            buttonstatus = false;
+            console_string = second;
+        }
+
+        $.post(
+            '/button/' + num, {
+                state: buttonstatus
+            },
+            function () {
+                console.log("Button Toggle: " + num + " - " + console_string);
+            }
+        );
+		return true;
+    });
+
+}
+
+function makeToggleSwitch(num,label,first,second) {
+	var buttonsetName = "radio"+label;
+	$('<text>'+label+'</text>').appendTo("#radio_controls");
+	
+	var myItems = [];
+	myItems.push('<div id="');
+	myItems.push(buttonsetName);
+	myItems.push('">');
+	myItems.push('<form>');
+	myItems.push('<input type="radio" data-vrpn-button-state="true" name="radio" id="trueradio' + label + '"><label for="trueradio' + label + '">');
+	myItems.push(first);
+	myItems.push('</label>');
+	myItems.push('<input type="radio" data-vrpn-button-state="false" name="radio" id="falseradio' + label + '" checked="checked"><label for="falseradio' + label + '">');
+	myItems.push(second);
+	myItems.push('</label>');
+	myItems.push('</form>');
+	myItems.push('</div>');
+	var buttonset_div = $(myItems.join("")).appendTo("#radio_controls");
+	setToggleSwitchBehavior(buttonset_div,num,buttonsetName,first,second);
+}
+
 function reportAnalog(num, val) {
 	$.post(
 		'/analog/' + num,
@@ -31,15 +79,41 @@ function setButtonBehavior(elt, num) {
 		);
 		$(this).data("pressed", false)
 	});
-	/// Not worth my time right now to handle clicking, leaving, and coming back.
+	// Not worth my time right now to handle clicking, leaving, and coming back.
 }
 
 function makeButton(num,label) {
-	setButtonBehavior($('<input name="button' + num + '" type="button" value="' + label + '">').appendTo("#button_controls"), num);
+	setButtonBehavior($('<input data-vrpn-button="'+num+'" name="button' + num + '" type="button" value="' + label + '">').appendTo("#button_controls"), num);
+}
+
+function setAnalogSliderBehavior($label,$slider,label,channel,min,max) {
+	var min = min || 0;
+	var max = max || 100;
+	$slider.slider(
+	{
+		value:min,
+		range: "min",
+		min: min,
+		max: max,
+		step: 1,
+		slide: function( event, ui ) {
+		  $label.html(label+": "+ui.value );
+		  reportAnalog(channel,ui.value);
+	}
+	});
+	$label.html(label+": "+min);
+
+}
+
+function makeAnalogSlider(label,channel,min,max) {
+	$label = $('<div id="'+label+'" style="margin-bottom:10px"></div>').appendTo("#slider_controls");
+	$slider = $('<div id="'+label+channel+'" style="margin-bottom:25px"></div>').appendTo("#slider_controls");
+	setAnalogSliderBehavior($label,$slider,label,channel,min,max);
+
 }
 
 function makeAnalogInput(label,defaultVal,channel) {
-	$input_field = $('<input id="'+label+'" type="text" value="' + defaultVal + '">').appendTo("#analog_controls");
+	$input_field = $('<input id="'+label+'" type="text" value="' + defaultVal + '" style="display: inline-block;">').appendTo("#analog_controls");
 	$submit_button = $('<input id="'+label+'Submit" type="submit" value="Update ' + label + '">').appendTo("#analog_controls");
 	setAnalogBehavior($submit_button, $input_field, channel);
 }
