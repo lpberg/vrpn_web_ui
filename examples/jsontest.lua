72885ab5147f09e40f13322287002c87028ccce6
Leif Berg <lpberg@iastate.edu>
2013-09-16 13:29:13 -0500

    updated VRJuggLua example with analog, text input, and button press action frames

--- examples/jsontest.lua
+++ examples/jsontest.lua
@@ -3,41 +3,87 @@ require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
 vrjKernel.loadConfigFile(vrjLua.findInModelSearchPath([[json_vrpn.jconf]]))
 
-function round(num, idp)
-  local mult = 10^(idp or 0)
-  return math.floor(num * mult + 0.5) / mult
-end
-
-
-addButtonFrameActions = function()
-	for i = 0, 3, 1 do
-		Actions.addFrameAction(function()
-			local drawBtn = gadget.DigitalInterface("JSButton"..i)
+local function recieveString()
+	strings = gadget.StringInterface("JSText")
+	Actions.addFrameAction(function()
 			while true do
-				if drawBtn.justPressed then
-					print(i.." pressed")
+				newData = strings.data
+				if newData ~= "" then
+					print("New String Recieved: ", newData)
 				end
 				Actions.waitForRedraw()
 			end
 		end)
-	end
 end
 
-addAnalogFrameActions = function()
-	for i = 0, 1, 1 do
-		Actions.addFrameAction(function()
-			local drawBtn = gadget.AnalogInterface("JSAnalog"..i)
-			local first_data = drawBtn.data
+recieveString()
+
+local function recieveAnalog0()
+	local input = gadget.AnalogInterface("JSAnalog0")
+	Actions.addFrameAction(function()
+			local last_value = ""
 			while true do
-				if drawBtn.data ~= first_data then
-					first_data = drawBtn.data
-					print("Analog "..i.." reports: "..round(first_data*1000000,2))
+				local newData = input.data
+				if newData ~= nil and newData ~= last_value then
+					print("New Analog Value Recieved on JSAnalog0: ", newData)
+					last_value = newData
 				end
 				Actions.waitForRedraw()
 			end
 		end)
-	end
+end
+recieveAnalog0()
+
+local function recieveAnalog10()
+	local input = gadget.AnalogInterface("JSAnalog10")
+	Actions.addFrameAction(function()
+			local last_value = ""
+			while true do
+				local newData = input.data
+				if newData ~= nil and newData ~= last_value then
+					print("New Analog Value Recieved on JSAnalog10: ", newData)
+					last_value = newData
+				end
+				Actions.waitForRedraw()
+			end
+		end)
+end
+recieveAnalog10()
+
+
+local function doWhen(condition, actionToTake)
+	Actions.addFrameAction(function()
+			while true do
+				if condition() then
+					actionToTake()
+					Actions.waitForRedraw()
+				else
+					Actions.waitForRedraw()
+				end
+			end
+		end)
 end
 
-addButtonFrameActions()
-addAnalogFrameActions()
+function simplePrintFunction(mystr)
+	local f = function()
+		print(mystr)
+	end
+	return f
+end
+
+function buttonJustPressed(button)
+	local f = function()
+		return button.justPressed
+	end
+	return f
+end
+
+function buttonPressed(button)
+	local f = function()
+		return button.pressed
+	end
+	return f
+end
+
+doWhen(buttonPressed(gadget.DigitalInterface("JSButton1")), simplePrintFunction("button 1 is pressed!"))
+doWhen(buttonJustPressed(gadget.DigitalInterface("JSButton0")), simplePrintFunction("button 0 was just pressed!"))
